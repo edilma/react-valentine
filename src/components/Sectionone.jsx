8d2b5ccab4a6baf189d46c8c0200dc5296e51270
Edilma Riano <edilmaria@hotmail.com>
2023-02-05 21:34:02 -0500

    created react app for valentines day

---
+++ src/components/Sectionone.jsx
@@ -0,0 +1,13 @@
+
+
+export function Sectionone(){
+    return(
+        <section className="card-section">
+        <div ClassName="corner-card-large"> 
+        <img ClassName="heart-2" src={require("./images/heart-2.webp")} alt="valentine day heart"/></div>
+        <div ClassName="center-card"></div>
+        <div ClassName = "corner-card-short"><img src={require("./images/heart-1.webp")}/></div>
+        </section>
+
+    )
+}
---
+++ src/components/Sectiontwo.jsx
@@ -0,0 +1,10 @@
+export function Sectiontwo(){
+    return (
+        <section className="hpd-message" >
+        <div className="happy">happy</div>
+        <div className="valentines">Valentines</div>
+        <div className="day">Day </div>
+        <div><button onclick="generateheart()">Send Love</button></div>
+        </section>
+    )
+}
