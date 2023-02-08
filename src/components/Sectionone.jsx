73c8693f812f1fdd6932818352e2844b5d6b31ba
Edilma Riano <edilmaria@hotmail.com>
2023-02-07 19:56:59 -0500

    FIXED css valentine card

--- src/components/Sectionone.jsx
+++ src/components/Sectionone.jsx
@@ -4,9 +4,9 @@ export function Sectionone(){
     return(
         <section className="card-section">
         <div ClassName="corner-card-large"> 
-        <img ClassName="heart-2" src={require("./images/heart-2.webp")} alt="valentine day heart"/></div>
+            <img ClassName="heart-2" src={require("./images/heart-2.webp")} alt="valentine day heart"/></div>
         <div ClassName="center-card"></div>
-        <div ClassName = "corner-card-short"><img src={require("./images/heart-1.webp")} alt="red hearts"/></div>
+            <div ClassName = "corner-card-short"><img src={require("./images/heart-1.webp")} alt="red hearts"/></div>
         </section>
 
     )
